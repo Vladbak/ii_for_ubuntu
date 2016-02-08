de8380ee3afc829008a2d4437a8bd3349736ff3c
Thijs Wenker <me@thoys.nl>
2016-02-08 13:41:30 +0100

    fixup build win32

--- interface/src/Menu.cpp
+++ interface/src/Menu.cpp
@@ -169,7 +169,7 @@ Menu::Menu() {
     QObject* avatar = avatarManager->getMyAvatar();
 
     // Avatar > Attachments...
-    action = addActionToQMenuAndActionHash(avatarMenu, MenuOption::Attachments);
+    auto action = addActionToQMenuAndActionHash(avatarMenu, MenuOption::Attachments);
     connect(action, &QAction::triggered, [] {
         DependencyManager::get<OffscreenUi>()->show(QString("hifi/dialogs/AttachmentsDialog.qml"), "AttachmentsDialog");
     });
